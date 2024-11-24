88dae439c3c17e22d95fc17fe23c7e774f0d64e8
Sefak <sefakolcu@gmail.com>
2024-11-24 22:44:03 +0300

    first commit

---
+++ client.py
@@ -0,0 +1,53 @@
+import socket
+import threading
+import pyaudio
+import zlib
+
+CHUNK = 2048
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+
+audio = pyaudio.PyAudio()
+stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+output = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)
+
+SERVER = '164.92.237.144'
+PORT = 5000
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket.bind(('', 0))
+
+def send_audio():
+    while True:
+        try:
+            data = stream.read(CHUNK, exception_on_overflow=False)
+            if len(data) > 0:
+                compressed_data = zlib.compress(data)
+                client_socket.sendto(compressed_data, (SERVER, PORT))
+        except Exception as e:
+            print(f"Send Audio Error: {e}")
+
+def receive_audio():
+    while True:
+        try:
+            data, _ = client_socket.recvfrom(4096)
+            if len(data) > 0:
+                try:
+                    decompressed_data = zlib.decompress(data)
+                    output.write(decompressed_data)
+                except Exception as e:
+                    print(f"Decompression failed: {e}")
+        except Exception as e:
+            print(f"Receive Audio Error: {e}")
+
+threading.Thread(target=send_audio, daemon=True).start()
+threading.Thread(target=receive_audio, daemon=True).start()
+
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    print("Exiting...")
+    client_socket.close()
+    stream.close()
+    audio.terminate()
---
+++ server.py
@@ -0,0 +1,24 @@
+import socket
+import zlib
+
+HOST = '0.0.0.0'
+PORT = 5000
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind((HOST, PORT))
+
+print(f"Server listening on {HOST}:{PORT}")
+
+while True:
+    data, addr = server_socket.recvfrom(4096)
+    
+    if len(data) > 0:
+        try:
+            data = zlib.decompress(data)
+        except Exception as e:
+            print(f"Decompression failed: {e}")
+        
+        try:
+            compressed_data = zlib.compress(data)
+            server_socket.sendto(compressed_data, addr)
+        except Exception as e:
+            print(f"Failed to send data back to {addr}: {e}")
